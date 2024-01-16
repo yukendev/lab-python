980f6fe8ef5bf3c1b60f98866922d741dc37227d
yukendev <yuken0906dev@gmail.com>
2024-01-16 16:04:05 +0900

    add one_cell_analytics.py

---
+++ one_cell_analytics.py
@@ -0,0 +1,27 @@
+from analytics.rotation_radius import rotation_radius
+from analytics.rotation_direction import rotation_direction
+from analytics.rotation_correction import rotation_correction
+from analytics.rotation_animation import rotation_animation
+from analytics.rotation_x import rotation_x
+from analytics.rotation_y import rotation_y
+import pandas as pd
+import os
+
+# 解析対象のcell番号
+cell_number="1220_1_1"
+
+# 解析対象の重心位置データファイル
+file_path = f'./data/cell_{cell_number}.txt'
+
+# 結果出力ディレクトリ
+output_directory = f'./result/one_cell_analytics/{cell_number}/'
+os.makedirs(output_directory, exist_ok=True)
+
+print(f'処理を実行します: {file_path}')
+
+rotation_x(cell_number, file_path, output_directory)
+rotation_y(cell_number, file_path, output_directory)
+rotation_radius(cell_number, file_path, output_directory)
+rotation_direction(cell_number, file_path, output_directory)
+rotation_correction(cell_number, file_path, output_directory)
+rotation_animation(cell_number, file_path, output_directory, 100)
