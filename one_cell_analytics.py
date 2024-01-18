75d04a74fcefd2aa12122a407d379c2e462c7964
yukendev <yuken0906dev@gmail.com>
2024-01-18 17:46:05 +0900

    rotation_fit_circle

--- one_cell_analytics.py
+++ one_cell_analytics.py
@@ -4,6 +4,7 @@ from analytics.rotation_correction import rotation_correction
 from analytics.rotation_animation import rotation_animation
 from analytics.rotation_x import rotation_x
 from analytics.rotation_y import rotation_y
+from analytics.rotation_fit_circle import rotation_fit_circle
 import os
 
 # 解析対象のcell番号
@@ -12,8 +13,8 @@ cell_number="1220_1_5"
 fps=12500.0
 
 # {start_second}秒から{end_second}秒までを解析
-start_second=3
-end_second=5
+start_second=0.3
+end_second=0.7
 
 # {start_frame}フレームから{end_frame}フレームまでを解析
 start_frame = start_second * fps
@@ -28,9 +29,10 @@ os.makedirs(output_directory, exist_ok=True)
 
 print(f'処理を実行します: {file_path}')
 
+rotation_fit_circle(cell_number, file_path, output_directory, fps, int(start_frame), int(end_frame))
 rotation_x(cell_number, file_path, output_directory, fps, int(start_frame), int(end_frame))
 rotation_y(cell_number, file_path, output_directory, fps, int(start_frame), int(end_frame))
-rotation_radius(cell_number, file_path, output_directory, fps, int(start_frame), int(end_frame))
+# rotation_radius(cell_number, file_path, output_directory, fps, int(start_frame), int(end_frame))
 rotation_direction(cell_number, file_path, output_directory, fps, int(start_frame), int(end_frame))
-rotation_correction(cell_number, file_path, output_directory, fps, int(start_frame), int(end_frame))
+# rotation_correction(cell_number, file_path, output_directory, fps, int(start_frame), int(end_frame))
 # rotation_animation(cell_number, file_path, output_directory, fps)
